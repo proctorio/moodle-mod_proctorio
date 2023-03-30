f5ab45aead69cf4bfccfcfd2a832ead79900cff0
Nick Oravec <nikolao@proctorio.com>
2023-03-30 13:17:23 +0200

    Delete all. Create PR

--- lang/en/local_proctorio.php
+++
@@ -1,3 +0,0 @@
-<?php
-$string['pluginname'] = 'Proctorio services';
-?>
--- version.php
+++
@@ -1,5 +0,0 @@
-<?php
-$plugin->version = 2019052000;  // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2019052000; // Requires this Moodle version
-$plugin->component = 'local_proctorio'; // Full name of the plugin (used for diagnostics)
-?>
